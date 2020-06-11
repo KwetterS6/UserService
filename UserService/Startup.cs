21be843cb351339d88dc7fdd6f6ebfff5eac4e83
Wouter Vandenboorn <wouter04316@gmail.com>
2020-06-11 15:23:25 +0200

    added cors

--- UserService/Startup.cs
+++ UserService/Startup.cs
@@ -30,7 +30,8 @@ namespace UserService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           
+
+            services.AddCors();
             
             services.AddControllers();
             
